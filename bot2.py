451005103c44235b7d1827b70de7c8ba9bc538fc
ziombelPL <ziom5060@gmail.com>
2024-10-12 11:04:25 +0200

    bot2.py

---
+++ bot2.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from bot_logic import *
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def haslo(ctx, pass_lenght = 10):
+    await ctx.send(gen_pass(pass_lenght))
+
+bot.run("")
