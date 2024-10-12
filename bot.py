89637625e2b65565bddba2592f360fb59c95b8e7
ziombelPL <ziom5060@gmail.com>
2024-10-12 10:59:27 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from bot_logic import *
+# Zmienna intencje przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Cześć!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$hasło'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run("")
