e4e514af00a17817f85e5b859af782fbe566ae1a
ziombelPL <ziom5060@gmail.com>
2024-10-26 10:49:38 +0200

    Update bot2.py
    pomysly

--- bot2.py
+++ bot2.py
@@ -22,4 +22,16 @@ async def heh(ctx, count_heh = 5):
 async def haslo(ctx, pass_lenght = 10):
     await ctx.send(gen_pass(pass_lenght))
 
+@bot.command()
+async def pomysly(ctx):
+    pomysly = ["Ramki na zdjęcia z patyczków po lodach", "Pojemniki na długopisy z rolek po papierze toaletowym", "Świeczniki ze słoików", "Łańcuch z kolorowego papieru", 
+                "Ozdoby z masy solnej", "Breloczki z koralików i żyłki", "Doniczki z puszek po konserwach", "Ozdobne zakładki do książek", "Kolorowe mozaiki z potłuczonych płytek lub szkła",
+                "Organizer na biżuterię z kartonów po jajkach", "Ozdobne świece z resztek świec", "Kolorowe pompony z włóczki", "Torebki na prezenty z gazet lub starego papieru",
+                "Podstawki pod kubki z korków po winie", "Dywanik z pomponów", "Witraż z plastikowych butelek", "Lampiony z papieru", "Torba z koszulki", "Kolorowe śnieżynki z papieru",
+                "Kwiaty z filtrów do kawy", "Zegary z płyt winylowych", "Szkatułki z kartonowych pudełek", "Poduszki z jeansu", "Podkładki pod talerze z płyt CD",
+                "Ptaszki z filcu", "Zakładki do książek z kartonu i tasiemek", "Girlandy z kolorowego papieru", "Ozdobne słoiki na przybory", "Składane miseczki z papieru",
+                "Latawce z plastikowych torebek", "Kolaże z gazet i czasopism", "Piórniki z opakowań po chusteczkach", "Malowane kamienie", "Ozdoby świąteczne z masy solnej",
+                "Dzwonki wietrzne z muszelek"]
+    await ctx.send(random.choice(pomysly))
+
 bot.run("")
